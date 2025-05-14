53ec596d9d8269085efbdf9e12ec46f0006ed783
Yuyan-C <yuyan.chen2@mail.mcgill.ca>
2025-05-14 14:54:51 -0400

    feat: add weights downloading script

--- scripts/examples/train.sh
+++ scripts/examples/train.sh
@@ -8,7 +8,9 @@ python scripts/main.py \
     --optimizer.num_epochs 2 \
     --optimizer.warmup_epochs 1 \
     --optimizer.lr 0.01 \
-    --dataset.train.batch_size 512 \
+   --dataset.train.batch_size 32 \
+    --dataset.val.batch_size 32 \
+    --dataset.test.batch_size 32 \
     --num_gpus 1 --num_workers 1 \
     --merge_option merge \
     --run_dir example \
--- scripts/examples/train_with_aux_data.sh
+++ scripts/examples/train_with_aux_data.sh
@@ -7,13 +7,14 @@ python scripts/main.py \
     configs/networks/resnet50.yml \
     configs/pipelines/train/baseline.yml \
     configs/pipelines/train/train_oe.yml \
-    configs/preprocessors/base_preprocessor.yml \
    --network.pretrained True \
    --network.checkpoint weights/c-america_resnet50_baseline.pth \
     --optimizer.num_epochs 2 \
     --optimizer.warmup_epochs 1 \
     --optimizer.lr 0.01 \
-    --dataset.train.batch_size 512 \
+    --dataset.train.batch_size 32 \
+    --dataset.val.batch_size 32 \
+    --dataset.test.batch_size 32 \
     --num_gpus 1 --num_workers 1 \
     --merge_option merge \
     --seed 0
